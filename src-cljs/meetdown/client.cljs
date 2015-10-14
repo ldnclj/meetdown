057996076da9a92aeb45aede66b2b6fbe38141bc
Dean Pehrsson-Chapman <dean.chapman@jhc.co.uk>
2015-10-14 22:49:10 +0100

    Cleanup

--- src-cljs/meetdown/client.cljs
+++ src-cljs/meetdown/client.cljs
@@ -2,11 +2,10 @@
     (:require-macros
       [cljs.core.async.macros :as asyncm :refer (go go-loop)])
     (:require
-      ;;[sablono.core :as html :refer-macros [html]]
       [goog.events :as events]
       [goog.dom :as dom]
       [cljs.core.async :as async :refer (<! >! put! chan)]
-      [taoensso.sente  :as sente :refer (cb-success?)] ; <--- Add this
+      [taoensso.sente  :as sente :refer (cb-success?)]
       [taoensso.sente.packers.transit :as sente-transit]
       [cognitect.transit :as transit]))
 
@@ -14,11 +13,8 @@
 
 (let [{:keys [chsk ch-recv send-fn state]}
       (sente/make-channel-socket! "/chsk" {:type :auto :packer packer})]                      ;
-     (def chsk       chsk)
-     (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
-     (def chsk-send! send-fn) ; ChannelSocket's send API fn
-     (def chsk-state state)   ; Watchable, read-only atom
-     )
+  (def chsk-send! send-fn) ; ChannelSocket's send API fn
+  )
 
 (def r (transit/reader :json))
 

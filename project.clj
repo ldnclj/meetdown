fa2eb6183c4f7bd3d44551f0c98c05fa8f31cf53
J. Pablo Fernández <pupeno@pupeno.com>
2015-11-17 21:04:06 +0000

    Send events to the server as a :create-event message.

--- project.clj
+++ project.clj
@@ -21,7 +21,8 @@
                  [environ "1.0.0"]
                  [org.clojure/clojurescript "1.7.107" :scope "provided"]
                  [secretary "1.2.3"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cljs-ajax "0.5.1"]]
 
   :plugins [[lein-environ "1.0.0"]
             [lein-asset-minifier "0.2.2"]]
